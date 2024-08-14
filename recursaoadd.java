54d51e2bb20e56e04ba225e448f87b6e13ea354b
AllanGroisman <allan.groisman@edu.pucrs.br>
2024-08-14 20:40:19 -0300

    aula 14_08 falta 1 do readme

--- recursaoadd.java
+++ recursaoadd.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class recursaoadd {
     public static void main(String[] args) {
         
@@ -9,6 +11,20 @@ public class recursaoadd {
 
 
     System.out.println("Resultado convBase2: " + convBase2(25));
+
+    ArrayList<Integer> array = new ArrayList<>();
+    array.add(10);
+    array.add(1);
+    array.add(1);
+    array.add(100);
+    array.add(170);
+    System.out.println(array);
+    //System.out.println("Resultado de somaArray: " + somaArray(array));
+    System.out.println("Resultado de findBiggest: " + findBiggest(array));
+
+    System.out.println("Resultado de findSubStr: " + findSubStr("array","aaarraylist"));
+    System.out.println("Resultado de nrmDigit: " + nroDigit(0));
+
 
     }
 
@@ -41,6 +57,60 @@ public class recursaoadd {
             return "" + convBase2(n/2) + (n % 2);
         }
     } 
+   // Modele e implemente um método recursivo que calcule o somatório dos números contidos em um ArrayList de inteiros, passado como parâmetro.
+    public static int somaArray(ArrayList<Integer> array){
+        if(array.size() > 1){
+            int a = array.get(0);
+            int b = array.get(1);
+            array.remove(0);
+            array.remove(0);
+            array.add(0, a + b);
+            return somaArray(array);
+        }
+        else {
+            return array.get(0);
+        }
+    }
+    // Modele e implemente um método recursivo para encontrar o maior elemento de um ArrayList.
+    public static int findBiggest(ArrayList<Integer> ar) {
+        if(ar.size() > 1){
+            if(ar.get(0) >= ar.get(1)){
+                ar.remove(1);
+            }
+            else{
+                ar.remove(0);
+            }
+            return findBiggest(ar);
+        }
+        else{
+            return ar.get(0);
+        }
+    }
 
-    
+    // Implemente um método recursivo para determinar se um string ocorre dentro de outro.
+    public static boolean findSubStr(String str, String match){
+        //System.out.println("str: " + str + ". match: " + match);
+        if (str.length()< match.length()){
+            if(str.equals(match.substring(0, str.length()))){
+                return true;
+            }
+            return findSubStr(str, match.substring(1,match.length()-1));
+        }
+        return false;
+    }
+    //Faça um método recursivo que determina o número de dígitos de um inteiro.
+    public static int nroDigit(int n){
+        if(n < 10){
+            return 1;
+        }
+        else {
+            return 1 + nroDigit(n/10);
+        }
+    }
+
+    // Implemente um métodos que recebe um String e retorna um ArrayList com todas as permutações deste String.
+    public static ArrayList<String> permutations(String s){
+        
+        return new ArrayList<>();
+    }
 }
